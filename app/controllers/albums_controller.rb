3db92cfd2d4b5e97dbcc5034313c2ae479d1cf48
simesaba80 <simesaba1120@gmail.com>
2026-04-06 17:50:37 +0900

    feat: Album作成可能に #7

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -10,8 +10,14 @@ class AlbumsController < ApplicationController
 
   def create
     @album = Album.new(album_params)
+    @album.published_at = Time.current
     if @album.save
-      redirect_to @album, notice: "Album created successfully"
+      redirect_to albums_path, notice: "Album created successfully"
     end
   end
+
+  private
+    def album_params
+      params.expect(album: [ :title, :cover_image, :description, :status ])
+    end
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,4 @@
 class Album < ApplicationRecord
+  enum :status, { draft: 0, public_album: 1, private_album: 2 }
+  has_one_attached :cover_image
 end
