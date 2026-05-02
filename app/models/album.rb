330dcda254911787dda5e98400a12f2a099224d0
simesaba80 <simesaba1120@gmail.com>
2026-05-02 14:46:37 +0900

    fix: jsonの見直し #11

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,7 @@ class Album < ApplicationRecord
   enum :status, { draft: 0, public_album: 1, private_album: 2 }
   has_one_attached :cover_image
   has_many :photos, dependent: :destroy
+  accepts_nested_attributes_for :photos, allow_destroy: true
 
   def self.create_album(album_params, photo_params)
     album = Album.new(album_params)
@@ -24,7 +25,7 @@ class Album < ApplicationRecord
     album
   end
 
-  def self.update_album(album, album_params, photo_changes_params)
+  def self.update_album_old(album, album_params, photo_changes_params)
     ActiveRecord::Base.transaction do
       album.update!(album_params)
       puts "--------------------------------"
@@ -53,6 +54,13 @@ class Album < ApplicationRecord
       end
     end
 
+    album
+  end
+
+  def self.update_album(album, photos_params)
+    ActiveRecord::Base.transaction do
+      album.update!(photos_params)
+    end
     album
   end
 end
